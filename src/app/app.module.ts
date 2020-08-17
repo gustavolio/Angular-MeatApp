e6d23631cba2b3718420f203223e848bd4987809
Gustavo <g.cdcomp@gmail.com>
2020-08-17 18:39:21 -0300

    Pipes and Async 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,12 @@ import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 
-import { RestaurantsService } from './restaurants/restaurants.services'
+import { RestaurantsService } from './restaurants/restaurants.services';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 
 @NgModule({
@@ -22,7 +27,12 @@ import { RestaurantsService } from './restaurants/restaurants.services'
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    MenuItemComponent,
+    ShoppingCartComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/restaurants/restaurants.services.ts
+++ src/app/restaurants/restaurants.services.ts
@@ -5,10 +5,10 @@ import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 
 import { Injectable } from '@angular/core'
-import { Http } from '@angular/http'
+import { Http, Response } from '@angular/http'
 
 import { MEAT_API } from '../app.api'
-import {ErrorHandler} from '../app.error-handle'
+import { ErrorHandler } from '../app.error-handle'
 
 //Decorator is used for use another decorator inside them.
 @Injectable()
@@ -17,8 +17,19 @@ export class RestaurantsService {
   constructor(private http: Http) { }
 
   restaurants(): Observable<Restaurant[]> {
-
     return this.http.get(`${MEAT_API}/restaurants`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
+
+  restaurantsById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
   }
