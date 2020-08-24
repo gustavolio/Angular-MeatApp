bb3eea56b24285040ec6c7bc39580a414eac9f2a
Gustavo <g.cdcomp@gmail.com>
2020-08-24 10:19:26 -0300

    Rating Component

--- src/app/order-sumary/order-sumary.component.ts
+++ src/app/order-sumary/order-sumary.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'mt-order-sumary',
-  templateUrl: './order-sumary.component.html'
+    selector: 'mt-order-sumary',
+    templateUrl: './order-sumary.component.html'
 })
 export class OrderSumaryComponent implements OnInit {
 
-  constructor() { }
+    // rated:boolean = false;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
