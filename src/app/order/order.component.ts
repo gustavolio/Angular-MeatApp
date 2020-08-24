d21ec34e8763a4349e3924a52e2d679c3481c8ee
Gustavo <g.cdcomp@gmail.com>
2020-08-24 12:27:31 -0300

    Validate forms with - reactive Forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,6 +4,7 @@ import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
     selector: 'mt-order',
@@ -11,17 +12,50 @@ import { Router } from '@angular/router';
 })
 export class OrderComponent implements OnInit {
 
+    emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    numberPattern = /^[0-9]*$/;
+    orderForm: FormGroup;
     delivery: number = 8;
 
     paymentOptions: RadioOption[] = [
-        {label: "Dinheiro", value:"MON"},
-        {label: "Cartão de Débito", value:"DEB"},
-        {label: "Cartão Refeição", value:"REF"},
+        { label: "Dinheiro", value: "MON" },
+        { label: "Cartão de Débito", value: "DEB" },
+        { label: "Cartão Refeição", value: "REF" },
     ];
 
-    constructor(private orderService: OrderService, private route: Router) { }
+    constructor(
+        private orderService: OrderService,
+        private route: Router,
+        private formBuilder: FormBuilder
+    ) { }
 
     ngOnInit() {
+        this.orderForm = this.formBuilder.group({
+            name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+            email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+            emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+            address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+            number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+            optionalAddress: this.formBuilder.control(''),
+            paymentOption: this.formBuilder.control('', [Validators.required])
+        }, {
+            validator: OrderComponent.equalsTo
+        });
+    }
+
+    static equalsTo(order: AbstractControl): {[key:string]: boolean} {
+        const email = order.get('email');
+        const emailConfirmation = order.get('emailConfirmation');
+
+        if(!email || !emailConfirmation){
+            return undefined;
+        }
+
+        if(email.value !== emailConfirmation.value){
+            return {emailsNotMatch: true}
+        }
+
+        return undefined;
     }
 
     itemsValue(): number {
@@ -46,7 +80,7 @@ export class OrderComponent implements OnInit {
 
     checkOrder(order: Order) {
         order.orderItems = this.cartItems()
-            .map((item: CartItem)=>new OrderItem(item.quantity, item.menuItem.id));
+            .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id));
 
         this.orderService.checkOrder(order)
             .subscribe((orderId: string) => {
