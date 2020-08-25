642013fb4603287a47db5700d2f69b416a864bed
Gustavo <g.cdcomp@gmail.com>
2020-08-25 12:05:54 -0300

    Install Animations Module (BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
@@ -40,6 +41,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpModule,
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -22,3 +22,6 @@ import 'zone.js/dist/zone';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR.js';
 
+//Animations module
+import 'web-animations-js/web-animations.min.js';
+
