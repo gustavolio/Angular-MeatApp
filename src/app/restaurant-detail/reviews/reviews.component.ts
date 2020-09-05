1a7bef386cf8a7e213c985a5a91cba6e4a295896
Gustavo <g.cdcomp@gmail.com>
2020-09-05 12:02:18 -0300

    Item Menu and Reviews Animations

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { RestaurantsService } from 'app/restaurants/restaurants.services';
 import { ActivatedRoute } from '@angular/router';
-
 import { Observable } from 'rxjs/Observable';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
-  selector: 'mt-reviews',
-  templateUrl: './reviews.component.html'
+    selector: 'mt-reviews',
+    templateUrl: './reviews.component.html',
+    animations: [
+        trigger('reviewAppeared', [
+            state('ready', style({ opacity: 1 })),
+            transition('void => ready', [
+                style({ opacity: 0, transform: 'translateY(-20px)' }),
+                animate('300ms 0s ease-in')
+            ])
+        ])
+    ]
 })
 export class ReviewsComponent implements OnInit {
 
-  reviews?: Observable<any>;
+    reviewState = 'ready';
 
-  constructor(private restaurantService: RestaurantsService, private route: ActivatedRoute) { }
+    reviews?: Observable<any>;
 
-  ngOnInit() {
-    this.reviews = this.restaurantService.reviewsOfRestaurant(this.route.parent.snapshot.params['id']);
-  }
+    constructor(private restaurantService: RestaurantsService, private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        this.reviews = this.restaurantService.reviewsOfRestaurant(this.route.parent.snapshot.params['id']);
+    }
 
 }
