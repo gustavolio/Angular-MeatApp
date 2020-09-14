f89ccd59b7fa93efff6eca272b8b81e352eb9806
Gustavo <g.cdcomp@gmail.com>
2020-09-14 15:03:37 -0300

    Restaurant SeachBar, Refactory with Angular 4.3 and HttpClient

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,20 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Restaurant } from './restaurant/restaurant.model'
-import { RestaurantsService } from './restaurants.services'
+import { Restaurant } from './restaurant/restaurant.model';
+import { RestaurantsService } from './restaurants.services';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/observable/from';
 
 @Component({
-  selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+    selector: 'mt-restaurants',
+    templateUrl: './restaurants.component.html',
+    animations: [
+        trigger('togglesearch', [
+            state('hidden', style({
+                opacity: 0,
+                "max-height": "0px",
+            })),
+            state('visible', style({
+                opacity: 1,
+                "max-height": "70px",
+                "margin-top": "20px"
+            })),
+            transition('* => *', animate('200ms 0s ease-in-out'))
+        ])
+    ]
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[]
+    searchBarState = 'hidden'; 
+    restaurants: Restaurant[];
 
-  constructor(private restaurantsService: RestaurantsService) { }
+    searchForm: FormGroup;
+    searchControl: FormControl;
 
-  ngOnInit() {
-    this.restaurantsService.restaurants().
-      subscribe(restaurants => this.restaurants = restaurants)
-  }
+    constructor(private restaurantsService: RestaurantsService,
+                private fb: FormBuilder) { }
+
+    ngOnInit() {
+        this.searchControl = this.fb.control('');
+        this.searchForm = this.fb.group({
+            searchControl: this.searchControl
+        });
+
+        this.searchControl.valueChanges
+            .debounceTime(500) //Aguarda 500 milisegundos
+            .distinctUntilChanged() //Se a query for igual não modifica
+            .switchMap(searchTerm => this.restaurantsService
+                .restaurants(searchTerm) //Busca os restaurantes com base no seachTerm
+                .catch(error => Observable.from([])))
+            .subscribe(restaurants => this.restaurants = restaurants); //Stribui o resultado à váriavel restaurants do componente
+
+        this.restaurantsService.restaurants().
+            subscribe(restaurants => this.restaurants = restaurants);
+        
+    }
+
+    toggleSeach() {
+        this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
+    }
 }
--- src/app/restaurants/restaurants.services.ts
+++ src/app/restaurants/restaurants.services.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 
 import { Injectable } from '@angular/core'
-import { Http, Response } from '@angular/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
 
 import { MEAT_API } from '../app.api'
 import { ErrorHandler } from '../app.error-handle'
@@ -15,29 +15,27 @@ import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model'
 @Injectable()
 export class RestaurantsService {
 
-  constructor(private http: Http) { }
+    constructor(private http: HttpClient) { }
 
-  restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`)
-      .map(response => response.json())
-      .catch(ErrorHandler.handleError)
-  }
+    restaurants(search?: string): Observable<Restaurant[]> {
+        let params: HttpParams;
+        if(search) {
+            //O set precisa estar na mesma linha do new por que um obj HttpParent é imutavel
+            params = new HttpParams().set('q', search);;
+        }
+        //A utilização do 'params' com o atributo generico 'q' permite a busca no json-server.
+        return this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`, { params: params })
+    }
 
-  restaurantsById(id: string): Observable<Restaurant> {
-    return this.http.get(`${MEAT_API}/restaurants/${id}`)
-      .map(response => response.json())
-      .catch(ErrorHandler.handleError)
-  }
+    restaurantsById(id: string): Observable<Restaurant> {
+        return this.http.get<Restaurant>(`${MEAT_API}/restaurants/${id}`)
+    }
 
-  reviewsOfRestaurant(id: string): Observable<any> {
-    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
-      .map(response => response.json())
-      .catch(ErrorHandler.handleError)
-  }
+    reviewsOfRestaurant(id: string): Observable<any> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+    }
 
-  menuOfRestaurant(id: string): Observable<MenuItem[]> {
-    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
-      .map(response => response.json())
-      .catch(ErrorHandler.handleError)
-  }
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get<MenuItem[]>(`${MEAT_API}/restaurants/${id}/menu`)
+    }
 }
